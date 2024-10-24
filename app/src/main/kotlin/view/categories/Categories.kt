bcbc8463cd558aed5915a7c90a47ae4780d5a29d
Felipe Gandra <41455881+fgandraf@users.noreply.github.com>
2024-10-24 14:10:49 -0300

    feat: add scrollbar for categories (#5)

--- app/src/main/kotlin/view/categories/Categories.kt
+++ app/src/main/kotlin/view/categories/Categories.kt
@@ -1,8 +1,8 @@
 package view.categories
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
@@ -12,13 +12,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.unit.dp
+import ui.shared.AddressView
+import ui.shared.SearchBar
 import view.categories.components.AddButton
 import view.categories.components.ListItem
 import view.categories.components.ListTypeItem
-import ui.shared.AddressView
-import ui.shared.SearchBar
-import ui.theme.Lime700
-import ui.theme.Red400
 
 @Composable
 fun Categories(){
@@ -54,8 +52,8 @@ fun Categories(){
                     //===== FIRST COLUMN
                     Row(modifier = Modifier.weight(2f).fillMaxHeight()) {
                         Column(modifier = Modifier.padding(top = 35.dp)) {
-                            ListTypeItem(icon = "gastos.svg", color = Red400, label = "Gastos") { }
-                            ListTypeItem(icon = "rendimentos.svg", color = Lime700, label = "Rendimentos") { }
+                            ListTypeItem(icon = "gastos.svg", color = MaterialTheme.colors.primary, label = "Gastos") { }
+                            ListTypeItem(icon = "rendimentos.svg", color = MaterialTheme.colors.primary, label = "Rendimentos") { }
                         }
                     }
                     Divider(modifier = Modifier.width(2.dp).fillMaxHeight())
@@ -63,147 +61,196 @@ fun Categories(){
 
                     //===== SECOND COLUMN
                     Row(modifier = Modifier.weight(3f).fillMaxHeight()) {
-                        Column(
-                            verticalArrangement = Arrangement.SpaceBetween,
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            modifier = Modifier.fillMaxSize()
-                        ) {
-                            Column(modifier = Modifier.fillMaxWidth().padding(top = 35.dp)) {
 
-                                ListItem(
-                                    icon = "food.svg",
-                                    color = Red400,
-                                    label = "Alimentação",
-                                    hasSubItems = true,
-                                    onIconClick = {},
-                                    onContainerClick = {}
-                                )
+                        val scrollState = rememberScrollState()
+                        Box(modifier = Modifier.fillMaxSize()){
 
-                                ListItem(
-                                    icon = "shop.svg",
-                                    color = Red400,
-                                    label = "Compras",
-                                    onIconClick = {},
-                                    onContainerClick = {},
-                                    onTrailingClick = {}
-                                )
+                            Column(verticalArrangement = Arrangement.SpaceBetween) {
 
-                                ListItem(
-                                    icon = "education.svg",
-                                    color = Red400,
-                                    label = "Educação",
-                                    onContainerClick = {},
-                                    onIconClick = {},
-                                    onTrailingClick = {}
-                                )
+                                Column(
+                                    verticalArrangement = Arrangement.SpaceBetween,
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .weight(1f)
+                                        .verticalScroll(scrollState)
+                                        .padding(end = 12.dp)
+                                ) {
+                                    Column(modifier = Modifier.fillMaxWidth().padding(top = 35.dp)) {
 
-                                ListItem(
-                                    icon = "fun.svg",
-                                    color = Red400,
-                                    label = "Lazer",
-                                    hasSubItems = true,
-                                    onContainerClick = {},
-                                    onIconClick = {}
-                                )
+                                        ListItem(
+                                            icon = "food.svg",
+                                            color = MaterialTheme.colors.primary,
+                                            label = "Alimentação",
+                                            hasSubItems = true,
+                                            onIconClick = {},
+                                            onContainerClick = {}
+                                        )
 
-                                ListItem(
-                                    icon = "house.svg",
-                                    color = Red400,
-                                    label = "Moradia",
-                                    onContainerClick = {},
-                                    onIconClick = {},
-                                    onTrailingClick = {}
-                                )
+                                        ListItem(
+                                            icon = "shop.svg",
+                                            color = MaterialTheme.colors.primary,
+                                            label = "Compras",
+                                            onIconClick = {},
+                                            onContainerClick = {},
+                                            onTrailingClick = {}
+                                        )
 
-                                ListItem(
-                                    icon = "truck.svg",
-                                    color = Red400,
-                                    label = "Operacionais",
-                                    hasSubItems = true,
-                                    onContainerClick = {},
-                                    onIconClick = {}
-                                )
+                                        ListItem(
+                                            icon = "education.svg",
+                                            color = MaterialTheme.colors.primary,
+                                            label = "Educação",
+                                            onContainerClick = {},
+                                            onIconClick = {},
+                                            onTrailingClick = {}
+                                        )
 
-                                ListItem(
-                                    icon = "question.svg",
-                                    color = Red400,
-                                    label = "Outros",
-                                    onContainerClick = {},
-                                    onIconClick = {},
-                                    onTrailingClick = {}
-                                )
+                                        ListItem(
+                                            icon = "fun.svg",
+                                            color = MaterialTheme.colors.primary,
+                                            label = "Lazer",
+                                            hasSubItems = true,
+                                            onContainerClick = {},
+                                            onIconClick = {}
+                                        )
 
-                                ListItem(
-                                    icon = "medicine.svg",
-                                    color = Red400,
-                                    label = "Saúde",
-                                    onContainerClick = {},
-                                    onIconClick = {},
-                                    onTrailingClick = {}
-                                )
+                                        ListItem(
+                                            icon = "house.svg",
+                                            color = MaterialTheme.colors.primary,
+                                            label = "Moradia",
+                                            onContainerClick = {},
+                                            onIconClick = {},
+                                            onTrailingClick = {}
+                                        )
 
-                                ListItem(
-                                    icon = "weight.svg",
-                                    color = Red400,
-                                    label = "Taxas e Impostos",
-                                    hasSubItems = true,
-                                    onContainerClick = {},
-                                    onIconClick = {}
-                                )
+                                        ListItem(
+                                            icon = "truck.svg",
+                                            color = MaterialTheme.colors.primary,
+                                            label = "Operacionais",
+                                            hasSubItems = true,
+                                            onContainerClick = {},
+                                            onIconClick = {}
+                                        )
 
-                                ListItem(
-                                    icon = "car.svg",
-                                    color = Red400,
-                                    label = "Transporte",
-                                    onContainerClick = {},
-                                    onIconClick = {},
-                                    onTrailingClick = {}
-                                )
+                                        ListItem(
+                                            icon = "question.svg",
+                                            color = MaterialTheme.colors.primary,
+                                            label = "Outros",
+                                            onContainerClick = {},
+                                            onIconClick = {},
+                                            onTrailingClick = {}
+                                        )
 
-                                ListItem(
-                                    icon = "clothes.svg",
-                                    color = Red400,
-                                    label = "Vestimentas",
-                                    onContainerClick = {},
-                                    onIconClick = {},
-                                    onTrailingClick = {}
-                                )
-                            }
-                            Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.End) {
-                                Row(modifier = Modifier.width(130.dp), horizontalArrangement = Arrangement.Center) {
-                                    AddButton { }
+                                        ListItem(
+                                            icon = "medicine.svg",
+                                            color = MaterialTheme.colors.primary,
+                                            label = "Saúde",
+                                            onContainerClick = {},
+                                            onIconClick = {},
+                                            onTrailingClick = {}
+                                        )
+
+                                        ListItem(
+                                            icon = "weight.svg",
+                                            color = MaterialTheme.colors.primary,
+                                            label = "Taxas e Impostos",
+                                            hasSubItems = true,
+                                            onContainerClick = {},
+                                            onIconClick = {}
+                                        )
+
+                                        ListItem(
+                                            icon = "car.svg",
+                                            color = MaterialTheme.colors.primary,
+                                            label = "Transporte",
+                                            onContainerClick = {},
+                                            onIconClick = {},
+                                            onTrailingClick = {}
+                                        )
+
+                                        ListItem(
+                                            icon = "clothes.svg",
+                                            color = MaterialTheme.colors.primary,
+                                            label = "Vestimentas",
+                                            onContainerClick = {},
+                                            onIconClick = {},
+                                            onTrailingClick = {}
+                                        )
+                                    }
+                                }
+
+                                Column(modifier = Modifier.fillMaxWidth().height(40.dp).padding(end = 10.dp), horizontalAlignment = Alignment.End, verticalArrangement = Arrangement.Bottom) {
+                                    Row(modifier = Modifier.width(130.dp), horizontalArrangement = Arrangement.Center) {
+                                        AddButton { }
+                                    }
                                 }
                             }
+
+                            VerticalScrollbar(
+                                adapter = rememberScrollbarAdapter(scrollState),
+                                modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight().padding(vertical = 3.dp).clip(CircleShape).background(MaterialTheme.colors.primaryVariant.copy(alpha = 0.3F))
+                            )
                         }
                     }
-                    Divider(modifier = Modifier.width(2.dp).fillMaxHeight())
 
 
                     //===== THIRD COLUMN
                     Row(modifier = Modifier.weight(3f).fillMaxHeight()) {
-                        Column(
-                            verticalArrangement = Arrangement.SpaceBetween,
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            modifier = Modifier.fillMaxSize()
-                        ) {
-                            Column(modifier = Modifier.fillMaxWidth().padding(top = 35.dp)) {
-                                ListItem(
-                                    label = "Conveniência",
-                                    color = Red400,
-                                    onContainerClick = {},
-                                    onIconClick = {})
-                                ListItem(label = "Lanchonete", color = Red400, onContainerClick = {}, onIconClick = {})
-                                ListItem(label = "Restaurante", color = Red400, onContainerClick = {}, onIconClick = {})
-                                ListItem(label = "Outros", color = Red400, onContainerClick = {}, onIconClick = {})
-                            }
-                            Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.End) {
-                                Row(modifier = Modifier.width(130.dp), horizontalArrangement = Arrangement.Center) {
-                                    AddButton { }
+
+                        val scrollState = rememberScrollState()
+                        Box(modifier = Modifier.fillMaxSize()) {
+
+                            Column(verticalArrangement = Arrangement.SpaceBetween) {
+
+                                Column(
+                                    verticalArrangement = Arrangement.SpaceBetween,
+                                    horizontalAlignment = Alignment.CenterHorizontally,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .weight(1f)
+                                        .verticalScroll(scrollState)
+                                        .padding(end = 12.dp)
+                                ) {
+                                    Column(modifier = Modifier.fillMaxWidth().padding(top = 35.dp)) {
+                                        ListItem(
+                                            label = "Conveniência",
+                                            color = MaterialTheme.colors.primary,
+                                            onContainerClick = {},
+                                            onIconClick = {})
+                                        ListItem(
+                                            label = "Lanchonete",
+                                            color = MaterialTheme.colors.primary,
+                                            onContainerClick = {},
+                                            onIconClick = {})
+                                        ListItem(
+                                            label = "Restaurante",
+                                            color = MaterialTheme.colors.primary,
+                                            onContainerClick = {},
+                                            onIconClick = {})
+                                        ListItem(
+                                            label = "Outros",
+                                            color = MaterialTheme.colors.primary,
+                                            onContainerClick = {},
+                                            onIconClick = {})
+                                    }
+                                }
+
+                                Column(modifier = Modifier.fillMaxWidth().height(40.dp).padding(end = 10.dp), horizontalAlignment = Alignment.End, verticalArrangement = Arrangement.Bottom){
+                                    Row(
+                                        modifier = Modifier.width(130.dp),
+                                        horizontalArrangement = Arrangement.Center
+                                    ) {
+                                        AddButton { }
+                                    }
                                 }
                             }
+
+                            VerticalScrollbar(
+                                adapter = rememberScrollbarAdapter(scrollState),
+                                modifier = Modifier.align(Alignment.CenterEnd).fillMaxHeight().padding(vertical = 3.dp).clip(CircleShape).background(MaterialTheme.colors.primaryVariant.copy(alpha = 0.3F))
+                            )
                         }
                     }
-                    Divider(modifier = Modifier.width(2.dp).fillMaxHeight())
                 }
             }
         }
